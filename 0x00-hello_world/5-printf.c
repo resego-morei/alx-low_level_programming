c0491cf4c7361a881ba9630258e1428c973b73e8
resego-morei <resego.morei@gmail.com>
2022-09-08 17:27:10 -0700

    Task 5 commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print string in printf function
+ *
+ * Description: using main function
+ * this program prints with proper grammar, but the outcome is a piece of art,
+ * Retutn: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
