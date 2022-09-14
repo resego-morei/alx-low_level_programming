28561d729572bd13dc7c0fa78d3a5cf82a687010
resego-morei <resego.morei@gmail.com>
2022-09-13 20:48:40 -0700

    add.c commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - Addition of two integers
+ * @num1: first integer to be added
+ * @num2: second integer to be added
+ *
+ * Return: result of the addition
+ */
+int add(int num1, int num2)
+{
+	int sum = num1 + num2;
+	
+	return (sum);
+}
