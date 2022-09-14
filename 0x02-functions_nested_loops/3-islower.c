3163a8c9eb7bb268a940398b8836c53e581c2a01
resego-morei <resego.morei@gmail.com>
2022-09-13 18:43:05 -0700

    3-islower.c commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _islower - function to check for lower character
+ * @c: is the int that will use for the argument of the function
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a')
+	{
+		return (1);
+	}
+	else if (c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
