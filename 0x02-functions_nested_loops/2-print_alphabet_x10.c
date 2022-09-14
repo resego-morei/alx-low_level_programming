2d0ff835a7d774c205e2e93a1cea06e4e9ddba56
resego-morei <resego.morei@gmail.com>
2022-09-13 18:26:53 -0700

    2-print_alphabet_x10.c commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,13 @@
  *
  * Return: 0
  */
-
 void print_alphabet_x10(void)
 {
-	char ch = 'a', i = 0;
+	char ch, i;
 
-	for(;i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for(; ch <= 'z'; ch++)
+		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
 		}
