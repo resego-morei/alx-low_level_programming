ef33bfbbb60dcd929eadcec20a731441166bdd70
resego-morei <resego.morei@gmail.com>
2022-09-13 17:18:54 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _putchar -write the character c to stdout
+ * @c: character to be print
+ * Return: On success 1
+ * On error: -1 is returned, and errno is set appropriately
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+void num(int r, int n);
+void print_time_table(int n);
+void print_to_98(int n);
+int add(int a, int b);
+void time_table(void);
+void jack_bauer(void);
+int print_last_digit(int);
+int _abs(int);
+int print_sign(int n);
+
+#endif
