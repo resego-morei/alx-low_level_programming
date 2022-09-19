6afae5fbde707dbefd8658d7c34f5066e513e248
resego-morei <resego.morei@gmail.com>
2022-09-19 15:35:41 -0700

    Task 10 commit

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _atoi - function that convert a string to an integer.
+ *
+ * @s: entry string of the function
+ */
+
+int _atoi(char *s)
+{
+	int i = 1, n = 0;
+
+	char* temp = s;
+
+	char lower = '0', upper = '9';
+
+	for (; (*temp < lower || *temp > upper) && *temp != '\0';)
+	{
+		if (*temp == '-')
+		{
+			i *= -1;
+		}
+		temp += 1;
+	}
+	if (*temp != '\0')
+	{
+		do {
+			n = 10 * n + (*temp - '0');
+			temp += 1;
+		} while (*temp >= lower && *temp <= upper);
+	}
+	return (n * i);
+}
