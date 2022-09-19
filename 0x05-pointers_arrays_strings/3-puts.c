ca509ed48a02375387bea2da0d867f860906e426
resego-morei <resego.morei@gmail.com>
2022-09-19 12:52:23 -0700

    Task 3 correction

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,20 +1,17 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
-
 /**
  * _puts - function that prints a string, followed by a new line, to stdout.
  *
  * @str: the input string
  *
  */
-
 void _puts(char *str)
 {
 	int index;
        
 	int len = strlen(str);
-
 	for (index = 0; index < len; index++)
 	{
 		putchar(str[index]);
