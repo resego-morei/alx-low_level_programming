704ea0893a58e972a3e0a61a86de1e944294bf7b
resego-morei <resego.morei@gmail.com>
2022-09-19 12:41:04 -0700

    puts commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout.
+ *
+ * @str: the input string
+ *
+ */
+
+void _puts(char *str)
+{
+	int index;
+       
+	int len = strlen(str);
+
+	for(index = 0; index <= len; index++)
+	{
+		_putchar(str[index]);
+	}
+	_putchar('\n');
+}
