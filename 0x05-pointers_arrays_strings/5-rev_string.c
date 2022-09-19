97d1cc002a916cb6fea9b96ebe8a4daeeca68ce2
resego-morei <resego.morei@gmail.com>
2022-09-19 13:25:46 -0700

    Task 5 commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include<stdio.h>
+#include<string.h>
+
+/**
+ * rev_string - function that reverses a string.
+ *
+ * @s: input string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	strrev(s);
+}
