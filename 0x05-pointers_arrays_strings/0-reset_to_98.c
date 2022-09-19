afe23dc24a84ac2c56b38b52dc3f70afe923d9da
resego-morei <resego.morei@gmail.com>
2022-09-19 12:12:38 -0700

    swap function commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,6 @@
 /**
  *reset_to_98 - function that takes a pointer to an int as parameter and updates the value it points to to 98.
  * @n: entry to be reset to 98.
- * Return: Always return 0
  */
 void reset_to_98(int *n)
 {
