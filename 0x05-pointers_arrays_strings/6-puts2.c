42b08d319943e5775a538530d4c33fddde26f01f
resego-morei <resego.morei@gmail.com>
2022-09-19 13:44:35 -0700

    Task 6 commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts2 -  function that prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ *
+ * @str: entry string to be printed out
+ */
+
+void puts2(char *str)
+{
+	int len = strlen(str);
+
+	int index = 0;
+
+	while (str[index] < len)
+	{
+		if (index % 2 == 0)
+		{
+			putchar(str[index]);
+		}
+		else
+		{
+			continue;
+		}
+		index++;
+	}
+	putchar('\n');
+}
