493a767659744672a14c4bff56d7c9437bb00645
resego-morei <resego.morei@gmail.com>
2022-09-19 12:26:00 -0700

    strlen.c commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - function that returns the length of a string.
+ *
+ * @s: entry of the string
+ *
+ * Return: len of the string
+ */
+
+int _strlen(char *s)
+{
+	int len = strlen(s);
+	return (len);
+}
