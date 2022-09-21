e314ea041190b7b52d92c788e62f09839e9f8514
resego-morei <resego.morei@gmail.com>
2022-09-21 08:56:10 -0700

    Task 0 commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat -  function that concatenates two strings
+ *
+ * @dest: pointer to destination of string
+ * @src: source of the string concated
+ *
+ * Return: Concrated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int index_dest;
+
+	for (index_dest = 0; dest[index_dest] != '\0'; index_dest++);
+
+	int index;
+
+	for (index = 0; src[index] != '\0'; index++)
+	{
+		dest[index_dest + index] = src[index];
+	}
+
+	dest[index_dest + index] = '\0';
+
+
+	return dest;
+}
