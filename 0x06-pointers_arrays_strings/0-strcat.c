1bb90acfff26150630777c0feb235171ad24f629
resego-morei <resego.morei@gmail.com>
2022-09-21 12:07:06 -0700

    correct correct

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,19 +11,19 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int index_dest;
+	int index_dest, len = 0;
 
-	for (index_dest = 0; dest[index_dest] != '\0'; index_dest++);
+	for (index_dest = 0; dest[index_dest] != '\0'; index_dest++)
+	{
+		len += 1;
+	}
 
 	int index;
 
 	for (index = 0; src[index] != '\0'; index++)
 	{
-		dest[index_dest + index] = src[index];
+		dest[len++] = src[index];
 	}
-
-	dest[index_dest + index] = '\0';
-
 
 	return (dest);
 }
