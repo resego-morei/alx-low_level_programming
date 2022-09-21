bb550996550adc82d33c056bd4f714cf6bbc159e
resego-morei <resego.morei@gmail.com>
2022-09-21 12:49:46 -0700

    Task 1 commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * strncat - function that concatenates two strings
+ *
+ * @dest - destination pointer
+ * @src - source pointer
+ * n - number of bytes from src
+ *
+ * Return -  pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index, i, len;
+
+	for (index = 0; dest[index] != '\0'; index++)
+	{
+		len++;
+	}
+
+	for (index = 0; src[index] != '\0' && index < n; index++)
+	{
+		dest[len++] = src[index];
+	}
+
+	return (dest);
+}
